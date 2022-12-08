114156fd36c16f41a1690d27b7934c2fe0987dd9
Francesco Palandra <palandra.1849712@studenti.uniroma1.it>
2022-12-08 23:42:09 +0100

    Classification

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
+import numpy as np
+import pandas as pd 
+
+
+def load_dataset():
+    #load dataset
+    return pd.read_csv("train_set.tsv", sep='\t', header=0)
+
+
+def main():
+    #load dataset
+    dataset = load_dataset()
+
+    #normalize dataset
+    
 
 
 
+if __name__ == '__main__':
+    main()
 
-if name == 'main'
+# train model
+
+#normalize dataset
+def normalize_dataset(dataset):
+    return dataset
